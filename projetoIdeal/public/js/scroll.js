0a7ff04db0ab3244dbb456db0aae75b939798fe3
LucasAST <freakfluk@gmail.com>
2021-04-14 09:21:48 -0300

    update 09:21

--- projetoIdeal/public/js/scroll.js
+++ projetoIdeal/public/js/scroll.js
@@ -1,10 +1,14 @@
-jQuery(document).ready(function ($) {
+$(document).ready(function(){
+  $(window).scroll(function(){
+   if ($(document).scrollTop() > 220) {
+       $('.aside').css("display", "block")
+   }else{
+       $('.aside').css("display","none")
+   }
 
-    if ($(document).scrollTop() > 100) {
-        $('.side').css("display", "block")
-    }else{
-        $('.aside').css("display","none")
-    }
+  })
+})
+  jQuery(document).ready(function ($) {
     $(".scroll").click(function (event) {
       event.preventDefault();
       $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 1100);
